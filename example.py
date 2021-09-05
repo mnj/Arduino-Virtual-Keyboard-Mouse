63311844ee4535a7b6a2add49c6d02458c168d77
Michael N. Jensen <michael@sys-adm.dk>
2021-09-05 12:19:19 +0200

    Update variable name for args, to be more clear.

--- example.py
+++ example.py
@@ -22,46 +22,46 @@ try:
 
         # Set screen resolution for the Absolute Mouse library
         command = 4 # Init mouse
-        sub_cmd1 = 2560     # Width
-        sub_cmd2 = 1440     # Height
+        cmd_arg1 = 2560     # Width
+        cmd_arg2 = 1440     # Height
         
         ser.write( startmarker.to_bytes(1, 'big') )
         ser.write( command.to_bytes(1, 'big') )
-        ser.write( sub_cmd1.to_bytes(2, 'big') )
-        ser.write( sub_cmd2.to_bytes(2, 'big') )
+        ser.write( cmd_arg1.to_bytes(2, 'big') )
+        ser.write( cmd_arg2.to_bytes(2, 'big') )
         ser.write( endmarker.to_bytes(1, 'big') )
 
-        print(f"Set mouse resolution to: {sub_cmd1}x{sub_cmd2}")
+        print(f"Set mouse resolution to: {cmd_arg1}x{cmd_arg2}")
 
         # Now move the mouse to 1000,1000
         command = 5 # Move mouse
-        sub_cmd1 = 1000      # x
-        sub_cmd2 = 1000      # y
+        cmd_arg1 = 1000      # x
+        cmd_arg2 = 1000      # y
         
         ser.write( startmarker.to_bytes(1, 'big') )
         ser.write( command.to_bytes(1, 'big') )
-        ser.write( sub_cmd1.to_bytes(2, 'big') )
-        ser.write( sub_cmd2.to_bytes(2, 'big') )
+        ser.write( cmd_arg1.to_bytes(2, 'big') )
+        ser.write( cmd_arg2.to_bytes(2, 'big') )
         ser.write( endmarker.to_bytes(1, 'big') )
 
         # Now send a right click
         command = 6 # Move mouse
-        sub_cmd1 = 3         # right click
+        cmd_arg1 = 3         # right click
         
         ser.write( startmarker.to_bytes(1, 'big') )
         ser.write( command.to_bytes(1, 'big') )
-        ser.write( sub_cmd1.to_bytes(1, 'big') )
+        ser.write( cmd_arg1.to_bytes(1, 'big') )
         ser.write( endmarker.to_bytes(1, 'big') )
 
         # Now set up, so it keeps pressing key 49 (= the 1 key), with a 50ms delay
         command = 7
-        sub_cmd1 = 49
-        sub_cmd2 = 50
+        cmd_arg1 = 49
+        cmd_arg2 = 50
 
         ser.write( startmarker.to_bytes(1, 'big') )
         ser.write( command.to_bytes(1, 'big') )
-        ser.write( sub_cmd1.to_bytes(1, 'big') )
-        ser.write( sub_cmd2.to_bytes(1, 'big') )
+        ser.write( cmd_arg1.to_bytes(1, 'big') )
+        ser.write( cmd_arg2.to_bytes(1, 'big') )
         ser.write( endmarker.to_bytes(1, 'big') )
         
 
